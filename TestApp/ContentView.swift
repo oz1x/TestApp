e119b5c7f958521714290b8c76617bc249a047e0
oz1x <ozzreborn@gmail.com>
2023-09-25 14:02:28 -0400

    Initial Commit

---
+++ TestApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestApp
+//
+//  Created by Oz on 9/25/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Oz on 9/25/23.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
